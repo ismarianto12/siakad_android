71687effa903ed079770cf387c08ca4b0003afda
aditya <aditya.djauhari@mncgroup.com>
2024-05-26 02:31:53 +0700

    updated ata

--- app/src/main/java/com/tes/arsipsurat/arsipActivity.java
+++ app/src/main/java/com/tes/arsipsurat/arsipActivity.java
@@ -1,14 +1,70 @@
 package com.tes.arsipsurat;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import androidx.appcompat.app.AppCompatActivity;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class arsipActivity extends AppCompatActivity {
 
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> userList;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_arsip);
+//
+//        listView = findViewById(R.id.listView);
+//        userList = new ArrayList<>();
+//        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, userList);
+//        listView.setAdapter(adapter);
+//
+//        new FetchUserData().execute();
     }
+
+    private class FetchUserData extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                URL url = new URL("https://randomuser.me/api/");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder response = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+                JSONObject jsonObject = new JSONObject(response.toString());
+                JSONArray results = jsonObject.getJSONArray("results");
+                for (int i = 0; i < results.length(); i++) {
+                    JSONObject user = results.getJSONObject(i);
+                    String fullName = user.getJSONObject("name").getString("first") + " " + user.getJSONObject("name").getString("last");
+                    userList.add(fullName);
+                }
+                reader.close();
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            adapter.notifyDataSetChanged();
+        }
+    }
+
 }
